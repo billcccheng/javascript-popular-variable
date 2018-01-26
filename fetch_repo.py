c2c5e7edac91159a883555feaab64d0e2a8150dd
Bill cheng <billcccheng@gmail.com>
2018-01-26 13:19:00 -0600

    used counter

--- fetch_repo.py
+++ fetch_repo.py
@@ -1,9 +1,9 @@
-import collections
 import json
 import os
 import re
 import requests
 
+from collections import Counter
 from git import Repo
 
 def fetch_popular_repo():
@@ -25,7 +25,7 @@ def clone_repo(url):
 
     return repo_dir
 
-def read_files(repo_dir, variable_dict):
+def read_files(repo_dir, var_counter):
     prog = re.compile('(?:const|var|let|function)\s+(\w+);?')
     directories = [repo_dir]
     while directories:
@@ -38,25 +38,23 @@ def read_files(repo_dir, variable_dict):
             if not _file.endswith(".js"): continue
             curr_file = parent_dir + '/' + _file
             with open(curr_file , 'r') as content_file:
-                analyze_content(content_file, prog, variable_dict)
+                analyze_content(content_file, prog, var_counter)
 
-def analyze_content(contents, prog, variable_dict):
+def analyze_content(contents, prog, var_counter):
     content = contents.read()
     matches = prog.findall(content)
-    # TODO:
-    # Plug the matches into the dictionary
     if matches:
-        matches = [ match for match in matches if len(match) > 1 ]
-        matches.sort()
-        variable_dict += matches
-        print(collections.Counter(variable_dict))
+        for match in matches:
+            if len(match) < 2: continue
+            var_counter[match] += 1
+        print(var_counter.most_common(10))
 
 def main():
-    variable_dict = []
+    var_counter = Counter()
     popular_repo_urls = fetch_popular_repo()
     for url in popular_repo_urls:
         repo_dir = clone_repo(url)
-        read_files(repo_dir, variable_dict)
+        read_files(repo_dir, var_counter)
 
 if __name__ == "__main__":
     main()
