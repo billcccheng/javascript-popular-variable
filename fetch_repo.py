da5f7f6d45c0bbe45736af2cec44c461f0c967fe
Bill cheng <billcccheng@gmail.com>
2018-01-26 10:05:11 -0600

    read in files and directories

--- fetch_repo.py
+++ fetch_repo.py
@@ -1,6 +1,6 @@
-import base64
 import json
 import os
+import re
 import requests
 
 from git import Repo
@@ -10,15 +10,21 @@ popular_variable_dict = {}
 def fetch_popular_repo():
     git_url = 'https://github.com/billcccheng/ptt-search.git'
     repo_dir = './repos/ptt-search'
+    prog = re.compile('(?:const|var|let)\s+(\w+);?')
+
     if not os.path.exists(repo_dir):
         os.makedirs(repo_dir)
         Repo.clone_from(git_url, repo_dir)
 
-
-    print(os.listdir(repo_dir))
-    with open('./repos/ptt-search/app/index.js', 'r') as content_file:
+    directories = []
+    for _file in os.listdir(repo_dir):
+        if os.path.isdir(repo_dir + '/' + _file):
+            directories.append(_file)
+    # print(directories)
+    with open('./repos/ptt-search/app/components/App.js', 'r') as content_file:
         content = content_file.read()
-        print(content)
+        match = prog.findall(content)
+        # print(match)
 
 def main():
     fetch_popular_repo()
