9b60550540a98edfd3864cbf6f5e2e3534eef006
Bill cheng <billcccheng@gmail.com>
2018-01-26 09:21:37 -0600

    added some function to fetch popluar repo

--- fetch_repo.py
+++ fetch_repo.py
@@ -1,11 +1,28 @@
-import requests
 import base64
 import json
+import os
+import requests
 
-url = 'https://api.github.com/repos/billcccheng/ptt-search/contents/'
-results = requests.get(url).json()
-paths = [result["path"] for result in results if "path" in result]
-print(paths)
-# print(json.dumps(res, indent=2, sort_keys=False))
+from git import Repo
 
+popular_variable_dict = {}
+
+def fetch_popular_repo():
+    git_url = 'https://github.com/billcccheng/ptt-search.git'
+    repo_dir = './repos/ptt-search'
+    if not os.path.exists(repo_dir):
+        os.makedirs(repo_dir)
+        Repo.clone_from(git_url, repo_dir)
+
+
+    print(os.listdir(repo_dir))
+    with open('./repos/ptt-search/app/index.js', 'r') as content_file:
+        content = content_file.read()
+        print(content)
+
+def main():
+    fetch_popular_repo()
+
+if __name__ == "__main__":
+    main()
 
