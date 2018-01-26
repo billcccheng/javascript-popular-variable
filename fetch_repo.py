18018a6fea3ed1e99ec6c240cb1c7b42def0172f
Bill cheng <billcccheng@gmail.com>
2018-01-26 13:11:28 -0600

    refactored code

--- fetch_repo.py
+++ fetch_repo.py
@@ -1,11 +1,10 @@
+import collections
 import json
 import os
 import re
 import requests
 
 from git import Repo
-
-popular_variable_dict = {}
 
 def fetch_popular_repo():
     popular_repo_urls = []
@@ -16,40 +15,48 @@ def fetch_popular_repo():
         if len(popular_repo_urls) == 100: break
     return popular_repo_urls
 
-def clone_repo(urls):
+def clone_repo(url):
+    find_repo_name = re.compile('\/([^\/]+).git$')
+    repo_name = find_repo_name.findall(url)[0]
+    repo_dir = './repos/' + repo_name
+    if not os.path.exists(repo_dir):
+        os.makedirs(repo_dir)
+        Repo.clone_from(url, repo_dir)
+
+    return repo_dir
+
+def read_files(repo_dir, variable_dict):
     prog = re.compile('(?:const|var|let|function)\s+(\w+);?')
-    for url in urls:
-        repo_name = re.findall('\/([^\/]+).git$', url)[0]
-        repo_dir = './repos/' + repo_name
-        if not os.path.exists(repo_dir):
-            os.makedirs(repo_dir)
-            Repo.clone_from(url, repo_dir)
+    directories = [repo_dir]
+    while directories:
+        parent_dir = directories.pop()
+        all_dir = next(os.walk(parent_dir))[1]
+        for _dir in all_dir:
+            if _dir == '.git': continue
+            directories.append(parent_dir + '/' + _dir)
+        for _file in os.listdir(parent_dir):
+            if not _file.endswith(".js"): continue
+            curr_file = parent_dir + '/' + _file
+            with open(curr_file , 'r') as content_file:
+                analyze_content(content_file, prog, variable_dict)
 
-        directories = [repo_dir]
-        while directories:
-            parent_dir = directories.pop()
-            all_dir = next(os.walk(parent_dir))[1]
-            for _dir in all_dir:
-                if _dir == '.git': continue
-                directories.append(parent_dir + '/' + _dir)
-            for _file in os.listdir(parent_dir):
-                if not _file.endswith(".js"): continue
-                curr_file = parent_dir + '/' + _file
-                with open(curr_file , 'r') as content_file:
-                    analyze_content(content_file, prog)
-
-def analyze_content(contents, prog):
+def analyze_content(contents, prog, variable_dict):
     content = contents.read()
-    match = prog.findall(content)
+    matches = prog.findall(content)
     # TODO:
     # Plug the matches into the dictionary
-    if match:
-        match.sort()
-        print(match)
+    if matches:
+        matches = [ match for match in matches if len(match) > 1 ]
+        matches.sort()
+        variable_dict += matches
+        print(collections.Counter(variable_dict))
 
 def main():
+    variable_dict = []
     popular_repo_urls = fetch_popular_repo()
-    clone_repo(popular_repo_urls)
+    for url in popular_repo_urls:
+        repo_dir = clone_repo(url)
+        read_files(repo_dir, variable_dict)
 
 if __name__ == "__main__":
     main()
